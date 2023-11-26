c48b03d685ee2619cfb6225b6c9e22ca7e396633
flyzero <fly.zero@foxmail.com>
2023-11-26 10:11:22 +0800

    style: 为 `chpasswd` 函数添加 doxygen 风格的注释

--- chpasswd.h
+++ chpasswd.h
@@ -4,6 +4,15 @@
 extern "C" {
 #endif
 
+/**
+ * @brief 修改用户密码
+ *
+ * @param root_path 根目录，相当于 chroot 的根目录
+ * @param username 用户名
+ * @param password 密码
+ * @return true 成功
+ * @return false 失败
+ */
 bool chpasswd(const char * root_path, const char * username, const char * password);
 
 #ifdef __cplusplus
